de2304470bc517db6236f00a75823bbf1e1aadb1
ShakalakaB <yunjie989@gmail.com>
2021-07-07 00:30:02 +0800

    Init project

---
+++ src/test/java/aldora/spring/configclient/SpringConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package aldora.spring.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
